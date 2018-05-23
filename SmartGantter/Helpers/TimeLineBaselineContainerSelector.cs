6cccccc712a307f3d84c9f253046de020139779c
Aleksandr Kazakov <aleksandr.kazakov@homag.com>
2018-05-23 15:53:20 +0200

    up

---
+++ SmartGantter/Helpers/TimeLineBaselineContainerSelector.cs
@@ -0,0 +1,21 @@
+﻿using SmartGantter;
+using Telerik.Windows.Controls.GanttView;
+using Telerik.Windows.Rendering.Virtualization;
+
+namespace SmartGantter
+{
+	public class TimeLineBaselineContainerSelector : DefaultTimeLineContainerSelector
+	{
+		protected static readonly ContainerTypeIdentifier BaselineContainerType = ContainerTypeIdentifier.FromType<BaselineContainer>();
+
+		public override ContainerTypeIdentifier GetContainerType(object item)
+		{
+			if (item is BaselineEventInfo)
+			{
+				return BaselineContainerType;
+			}
+
+			return base.GetContainerType(item);
+		}
+	}
+}
